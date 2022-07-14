2b4372c0c1416fb914dbac4bc5b9730b09d9c504
Reese Rivers <44736084+Fussmatte@users.noreply.github.com>
2022-07-13 20:44:07 -0400

    Update sourceedits.lua

--- rightclicktileset/sourceedits.lua
+++ rightclicktileset/sourceedits.lua
@@ -1,42 +1,42 @@
 sourceedits =
 {
-    ["func"] =
-    {
-        {
-            find = [[
+	["func"] =
+	{
+		{
+			find = [[
 function switchtileset()
 	local maxtileset = 4
 	if keyboard_eitherIsDown("shift") then
 ]],
-            replace = [[
+			replace = [[
 function switchtileset(rev)
-    rev = rev or false
-    local maxtileset = 4
-    if keyboard_eitherIsDown("shift") or rev then
+	rev = rev or false
+	local maxtileset = 4
+	if keyboard_eitherIsDown("shift") or rev then
 ]],
-            ignore_error = false,
-            luapattern = false,
-            allowmultiple = false,
-        },
-        {
-            find = [[
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		},
+		{
+			find = [[
 function switchtilecol()
 	if keyboard_eitherIsDown("shift") then
 ]],
-            replace = [[
+			replace = [[
 function switchtilecol(rev)
-    rev = rev or false
-    if keyboard_eitherIsDown("shift") or rev then
+	rev = rev or false
+	if keyboard_eitherIsDown("shift") or rev then
 ]],
-            ignore_error = false,
-            luapattern = false,
-            allowmultiple = false,
-        },
-    },
-    ["uis/maineditor/draw"] =
-    {
-        {
-            find = [[
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		},
+	},
+	["uis/maineditor/draw"] =
+	{
+		{
+			find = [[
 	if love.mouse.isDown("l") and nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-70, (6*16), 8+4) and not voided_metadata then -- -16-32-2-12-8 => -70
 		-- Switch tileset
 		switchtileset()
@@ -47,26 +47,28 @@ function switchtilecol(rev)
 		mousepressed = true
 	end
 ]],
-            replace = [[
-	if love.mouse.isDown("l") and nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-70, (6*16), 8+4) and not voided_metadata then -- -16-32-2-12-8 => -70
-		-- Switch tileset
-		switchtileset()
-		mousepressed = true
-    elseif love.mouse.isDown("r")  and nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-70, (6*16), 8+4) and not voided_metadata then
-        switchtileset(true)
-        mousepressed = true
-	elseif love.mouse.isDown("l") and nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-58, (6*16), 8+4) and not voided_metadata then -- -16-24-2-8-8 => 58
-		-- Switch tilecol
-		switchtilecol()
-		mousepressed = true
-    elseif love.mouse.isDown("r") and nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-58, (6*16), 8+4) and not voided_metadata then
-        switchtilecol(true)
-        mousepressed = true
+			replace = [[
+	if nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-70, (6*16), 8+4) and not voided_metadata then -- -16-32-2-12-8 => -70
+		if love.mouse.isDown("l") then
+			switchtileset()
+			mousepressed = true
+		elseif love.mouse.isDown("r") then
+			switchtileset(true)
+			mousepressed = true
+		end
+	elseif nodialog and not mousepressed and mouseon(love.graphics.getWidth()-(7*16)-1, love.graphics.getHeight()-58, (6*16), 8+4) and not voided_metadata then -- -16-24-2-8-8 => 58
+		if love.mouse.isDown("l") then
+			switchtilecol()
+			mousepressed = true
+		elseif love.mouse.isDown("r") then
+			switchtilecol(true)
+			mousepressed = true
+		end
 	end
 ]],
-            ignore_error = false,
-            luapattern = false,
-            allowmultiple = false,
-        },
-    },
+			ignore_error = false,
+			luapattern = false,
+			allowmultiple = false,
+		},
+	},
 }
